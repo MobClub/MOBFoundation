3b3aef22f3f4d5ec670fa4c6b963157b7053a169
oklyc <ok_lyc@126.com>
2018-12-21 16:00:12 +0800

    3.2.1 release

--- MOBFoundationEx.framework/Headers/MOBFTCPClient.h
+++ MOBFoundationEx.framework/Headers/MOBFTCPClient.h
@@ -8,6 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(unsigned int, MOBFTCPClientState)
+{
+    MOBFTCPClientStateNone             = 0,
+    MOBFTCPClientStateConnecting       = 1,
+    MOBFTCPClientStateConnected        = 2,
+    MOBFTCPClientStateClosing          = 3,
+    MOBFTCPClientStateClosed           = 4,
+};
+
 /**
  *  Socket连接处理器
  */
@@ -27,6 +36,19 @@ typedef void (^MOBFTCPClientDisconnectedHandler) (NSError *error);
  */
 typedef void (^MOBFTCPClientReceiveDataHandler) (NSData *data);
 
+
+@class MOBFTCPClient;
+
+@protocol MOBFTCPClientDelegate <NSObject>
+
+@optional
+- (void)tcpClient:(MOBFTCPClient *)tcpClient didState:(MOBFTCPClientState)state;
+- (void)tcpClient:(MOBFTCPClient *)tcpClient didReceiveData:(NSData *)data;
+- (void)tcpClient:(MOBFTCPClient *)tcpClient didFailWithError:(NSError *)error;
+- (void)tcpClient:(MOBFTCPClient *)tcpClient didCloseWithError:(NSError *)error;
+
+@end
+
 /**
  *  Socket实现
  */
@@ -41,6 +63,16 @@ typedef void (^MOBFTCPClientReceiveDataHandler) (NSData *data);
  *  端口
  */
 @property (nonatomic, readonly) UInt32 port;
+
+/**
+ * 设置连接超时时间
+ */
+@property (nonatomic, assign) UInt64 timeoutInterval;
+
+/**
+ * 设置代理
+ */
+@property (nonatomic, weak) id<MOBFTCPClientDelegate> delegate;
 
 /**
  *  初始化Socket
@@ -70,6 +102,14 @@ typedef void (^MOBFTCPClientReceiveDataHandler) (NSData *data);
  *  @param data 数据
  */
 - (void)sendData:(NSData *)data;
+
+/**
+ *  设置心跳数据 及 间隔多少秒发送一次心跳包,调用该方法后才会启动, PingData=nil||duration<10 取消自动心跳
+ *
+ *  @param data 发送心跳数据
+ *  @param duration 心跳包间隔
+ */
+- (void)setHeartbeatWithPingData:(NSData *)pingData duration:(UInt64)duration;
 
 /**
  *  已连接事件
